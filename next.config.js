0cf00313e7aab1d334b11e7410a4e0178d52520e
Matteo Enrico Ferri Bonvento <matteo.bonvento@sou.unifeob.edu.br>
2026-02-15 23:42:13 -0300

    SistemaPronto5

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    // !! PERIGO !!
-    // Isso permite que o build complete com sucesso mesmo se
-    // o seu projeto tiver erros de TypeScript.
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    // Também ignora erros de linting durante o build
-    ignoreDuringBuilds: true,
-  },
+  typescript: { ignoreBuildErrors: true }
 };
 
 export default nextConfig;
